bc26925358b43f28136b6fa739b12a8c847bdd95
Sean Grove <sean@bushi.do>
2019-05-27 14:11:40 -0700

    Update

--- index.js
+++ index.js
@@ -6,34 +6,42 @@ const axios = require("axios");
 const db = "https://replicate.npmjs.com";
 
 const upsertPackageWithAuthorMutation = `
-mutation upsertPackageWithAuthor($description: String, $id: String!, $license: String, $name: String!, $readme: String, $rev: String!, $authorEmail: String, $authorName: String) {
-  insert_npm_package(objects: {description: $description, id: $id, license: $license, name: $name, readme: $readme, rev: $rev, author: {data: {email: $authorEmail, name: $authorName}, on_conflict: {constraint: author_email_key, update_columns: [name]}}}, on_conflict: {constraint: npm_package_pkey, update_columns: [description, id, license, name, readme, rev]}) {
+mutation upsertPackageWithAuthor($description: String, $id: String!, $license: String, $name: String!, $readme: String, $rev: String!, $authorEmail: String, $authorName: String, $createdAt: timestamptz, $updatedAt: timestamptz) {
+  insert_npm_package(objects: {description: $description, id: $id, license: $license, name: $name, readme: $readme, rev: $rev, created_at: $createdAt, updated_at: $updatedAt, author: {data: {email: $authorEmail, name: $authorName}, on_conflict: {constraint: author_email_key, update_columns: [name]}}}, on_conflict: {constraint: npm_package_pkey, update_columns: [description, id, license, name, readme, rev]}) {
     affected_rows
   }
 }`;
 
 const upsertPackageWithoutAuthorMutation = `
-mutation upsertPackageWithoutAuthor($description: String, $id: String!, $license: String, $name: String!, $readme: String, $rev: String!) {
-  insert_npm_package(objects: {description: $description, id: $id, license: $license, name: $name, readme: $readme, rev: $rev}, on_conflict: {constraint: npm_package_pkey, update_columns: [description, id, license, name, readme, rev]}) {
+mutation upsertPackageWithoutAuthor($description: String, $id: String!, $license: String, $name: String!, $readme: String, $rev: String!, $createdAt: timestamptz, $updatedAt: timestamptz) {
+  insert_npm_package(objects: {description: $description, id: $id, license: $license, name: $name, readme: $readme, rev: $rev, created_at: $createdAt, updated_at: $updatedAt}, on_conflict: {constraint: npm_package_pkey, update_columns: [description, id, license, name, readme, rev]}) {
     affected_rows
   }
 }
-
 `;
+
+var counter = 0;
+const reqsInFlight = {};
 
 var dataHandler = function(data, done) {
   if (data.doc.name) {
     // <-- make sure the change is a change
     const npm = Normalize(data.doc);
-    console.log(
-      `${npm.name} [${npm.license}]: ${npm.description} by ${npm.author &&
-        npm.author.email}`
-    );
+    // console.log(
+    //   `${npm.name} [${npm.license}]: ${npm.description} by ${npm.author &&
+    //     npm.author.email}`
+    // );
     // console.log(npm);
 
     const license = (npm.license && npm.license.type) || npm.license;
 
+    const time = !!npm.time && {
+      createdAt: npm.time.created,
+      updatedAt: npm.time.modified
+    };
+
     const package = {
+      ...time,
       id: npm._id,
       rev: npm._rev,
       name: npm.name,
@@ -53,17 +61,30 @@ var dataHandler = function(data, done) {
       package.authorName = npm.author.name;
     }
 
+    const reqId = ++counter;
+    reqsInFlight["" + reqId] = true;
+
+    console.log(`Starting req ${reqId}: ${npm.name}`);
+
     axios
       .post("http://localhost:8080/v1/graphql", {
         query: query,
         variables: package
       })
       .then(function(response) {
+        // console.log(`\tFinished [success] req ${reqId}: ${npm.name}`);
+        delete reqsInFlight["" + reqId];
+
         // console.log(response);
         done();
       })
       .catch(function(error) {
-        console.log(error);
+        console.log(
+          `\tFinished [error: (${error.code})] req ${reqId}: ${npm.name}`
+        );
+        delete reqsInFlight["" + reqId];
+
+        // console.log(error);
         done();
       });
 
@@ -85,7 +106,14 @@ var configOptions = {
   include_docs: true,
   sequence: ".sequence",
   now: false,
-  concurrency: 5
+  concurrency: 100
 };
 
 changes(dataHandler, configOptions);
+
+setInterval(() => {
+  console.log(
+    "Current reqs in flight: ",
+    JSON.stringify(Object.keys(reqsInFlight).sort(), null, 2)
+  );
+}, 5000);
